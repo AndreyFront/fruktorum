8d07d4ace7b5cd24975a207b2d2b00847c51b916
AndreyFront <andrey-se-v@yandex.ru>
2023-10-22 11:23:52 +0300

    edit useFetchApi

--- app/middleware/useFetchApi.js
+++ app/middleware/useFetchApi.js
@@ -1,8 +1,7 @@
-import { useFetch, useRuntimeConfig } from "nuxt/app"
+import { useFetch } from "nuxt/app"
 
 export default function useFetchApi(url, options = {}) {
-    const config = useRuntimeConfig(),
-        baseUrl = 'https://devtwit8.ru/api/v1/page/?path='
+    const baseUrl = 'https://devtwit8.ru/api/v1/page/?path='
 
     return useFetch(baseUrl + url, {
         ...options,
